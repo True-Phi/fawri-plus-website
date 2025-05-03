a23aadf56d3f43113330d9b8e05b635a1012afbb
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-03 23:29:54 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,5 @@
 import '../css/main.css';
-import { IntlProvider } from 'react-intl';
-import en from '../public/locales/en.json';
-import ar from '../public/locales/ar.json';
-import { useLocale } from '../hooks/useLocale';
 
 export default function MyApp({ Component, pageProps }) {
-  const locale = useLocale();
-  const messages = locale === 'ar' ? ar : en;
-  return (
-    <div dir={locale === 'ar' ? 'rtl' : 'ltr'}>
-      <IntlProvider locale={locale} messages={messages}>
-        <Component {...pageProps} />
-      </IntlProvider>
-    </div>
-  );
+    return <Component {...pageProps} />;
 }
