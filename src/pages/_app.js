0862dfda6689e58d31ab7a4bcca41110ad4ab5ca
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-03 23:22:39 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import '../css/main.css';
 import { IntlProvider } from 'react-intl';
 import en from '../public/locales/en.json';
 import ar from '../public/locales/ar.json';
-import { useLocale } from '../src/hooks/useLocale';
+import { useLocale } from '../hooks/useLocale';
 
 export default function MyApp({ Component, pageProps }) {
   const locale = useLocale();
