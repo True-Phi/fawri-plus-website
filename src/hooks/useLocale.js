43936c3c78f4172a74e8e5574235fe78892762f7
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-03 23:04:45 +0100

    Create useLocale.js

---
+++ src/hooks/useLocale.js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export function useLocale() {
+  const { query } = useRouter();
+  const [locale, setLocale] = useState('en');
+  useEffect(() => {
+    const q = typeof query.lang === 'string' ? query.lang : null;
+    if (q === 'ar' || q === 'en') setLocale(q);
+    else if (navigator.language.startsWith('ar')) setLocale('ar');
+    else setLocale('en');
+  }, [query.lang]);
+  return locale;
+}
