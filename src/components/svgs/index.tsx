0b22e99adc2883abfc50c4e1f01e384b1ef110be
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-04 14:56:13 +0100

    Update index.tsx

--- src/components/svgs/index.tsx
+++ src/components/svgs/index.tsx
@@ -22,7 +22,6 @@ import ShoppingBag from './shopping-bag';
 import Twitter from './twitter';
 import Vimeo from './vimeo';
 import YouTube from './youtube';
-import WhatsApp from './whatsapp';
 
 export const iconMap = {
     arrowDown: ArrowDown,
@@ -49,5 +48,4 @@ export const iconMap = {
     twitter: Twitter,
     vimeo: Vimeo,
     youtube: YouTube
-    whatsapp: WhatsApp
 };
