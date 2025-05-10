bca033bb8175e98d24607e043e345e62173e9b2b
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-10 23:35:29 +0100

    Update _app.js to include LanguageSwitcher and i18n for Arabic support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '../css/main.css';
+// src/pages/_app.js
+import "../i18n"; // Import i18n to initialize it
+import LanguageSwitcher from "../components/LanguageSwitcher";
+import "../css/main.css"; // Keep your existing global CSS
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <LanguageSwitcher />
+      <Component {...pageProps} />
+    </>
+  );
 }
