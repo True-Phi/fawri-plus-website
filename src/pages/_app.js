2c25135a15c5957e7070e55bcde614f8fd1a3e9c
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 00:52:54 +0100

    Update _app.js revert

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,5 @@
-// src/pages/_app.js
-import "../i18n"; // Import i18n to initialize it
-import LanguageSwitcher from "../components/LanguageSwitcher";
-import "../css/main.css"; // Keep your existing global CSS
+import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <LanguageSwitcher />
-      <Component {...pageProps} />
-    </>
-  );
+    return <Component {...pageProps} />;
 }
