1da2b5fb5f759c60f8bfd73534d1947eb7ebbde9
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 20:35:13 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '../css/main.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+// helper that knows /ar vs. English
+import { isArabicPath } from '../utils/locale';
 
 export default function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
+    // Whenever the route changes, update <html lang dir="">
+    useEffect(() => {
+        const arabic = isArabicPath(router.asPath);
+        document.documentElement.lang = arabic ? 'ar' : 'en';
+        document.documentElement.dir = arabic ? 'rtl' : 'ltr';
+    }, [router.asPath]);
+
     return <Component {...pageProps} />;
 }
