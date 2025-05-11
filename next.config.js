b39f3210dd5a44762e94dbb818cccce7a0ebd5c2
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 18:25:39 +0100

    Update next.config.js Add i18n support

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
+// next.config.js
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    env: {
-        stackbitPreview: process.env.STACKBIT_PREVIEW
-    },
-    trailingSlash: true,
-    reactStrictMode: true,
-    allowedDevOrigins: [
-        '192.168.1.84'
-    ]
+  env: {
+    stackbitPreview: process.env.STACKBIT_PREVIEW
+  },
+  trailingSlash: true,
+  reactStrictMode: true,
+  allowedDevOrigins: ['192.168.1.84'],
+  // ── Add i18n support ─────────────────────────────────────────────────────────
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+    localeDetection: false
+  }
 };
 
 module.exports = nextConfig;
