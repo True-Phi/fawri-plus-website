4695aef916f2aa7b2b08b76861865f4a99d73177
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 01:33:11 +0100

    Update and rename i18n.ts to i18n.js Convert i18n.ts to i18n.js for consistency

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,4 @@
-// src/i18n.ts
+// src/i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
@@ -9,7 +9,7 @@ i18n.use(initReactI18next).init({
     en: { translation: en },
     ar: { translation: ar },
   },
-  lng: "en", // Default language
+  lng: "en",
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
