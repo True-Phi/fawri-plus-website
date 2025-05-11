0d96068af0bf9c3acf8939c34069068cabff33fc
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 18:43:31 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,5 @@
 import '../css/main.css';
-import { useRouter } from 'next/router';
-import Head from 'next/head';
-import Header from '../components/sections/Header';
-import Footer from '../components/sections/Footer';
 
 export default function MyApp({ Component, pageProps }) {
-  const { locale } = useRouter();
-
-  return (
-    <>
-      <Head>
-        {/* inform search engines and browsers */}
-        <meta httpEquiv="Content-Language" content={locale} />
-      </Head>
-      {/* wrap to set reading direction */}
-      <div lang={locale} dir={locale === 'ar' ? 'rtl' : 'ltr'}>
-        {/* locale‐specific header/footer */}
-        <Header {...pageProps.header} />
-        <Component {...pageProps} />
-        <Footer {...pageProps.footer} />
-      </div>
-    </>
-  );
+    return <Component {...pageProps} />;
 }
