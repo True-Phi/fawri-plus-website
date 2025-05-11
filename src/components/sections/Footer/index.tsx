294e3e51a1613a8b39e6cb2953e747cf4644b05e
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 16:41:57 +0100

    Update index.tsx

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -5,7 +5,6 @@ import classNames from 'classnames';
 import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
 import { Social, Action, Link } from '../../atoms';
 import ImageBlock from '../../blocks/ImageBlock';
-import LanguageSwitcher from '../../LanguageSwitcher'; // Import LanguageSwitcher
 
 export default function Footer(props) {
     const {
@@ -70,9 +69,6 @@ export default function Footer(props) {
                                     </li>
                                 ))}
                             </ul>
-                            <div className="mt-4">
-                                <LanguageSwitcher /> {/* Add LanguageSwitcher here */}
-                            </div>
                         </div>
                     )}
                 </div>
