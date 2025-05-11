c820211cb29e434b1c806087ed79256756290573
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 01:07:18 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '../css/main.css';
+// src/pages/_app.js
+import "../i18n";
+import LanguageSwitcher from "../components/LanguageSwitcher";
+import "../css/main.css";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <LanguageSwitcher />
+      <Component {...pageProps} />
+    </>
+  );
 }
