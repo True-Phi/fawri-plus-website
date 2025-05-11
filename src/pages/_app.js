751b94140b30d423daa02ec63afd9ff5d895dd97
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-11 21:07:24 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,16 @@
 import '../css/main.css';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-
-// helper that knows /ar vs. English
 import { isArabicPath } from '../utils/locale';
 
 export default function MyApp({ Component, pageProps }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    // We still set <html lang> so SEO/accessibility can see Arabic,
-    // but we *always* keep dir="ltr".
+  useEffect(() => {
+    // Keep LTR but still tag Arabic pages for SEO
     document.documentElement.lang = isArabicPath(router.asPath) ? 'ar' : 'en';
     document.documentElement.dir  = 'ltr';
   }, [router.asPath]);
 
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
