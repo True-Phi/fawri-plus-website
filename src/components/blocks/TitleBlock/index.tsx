4bfa13b4718d845c3a5a277719c5ccaee6ceb5ab
True-Phi <136710653+True-Phi@users.noreply.github.com>
2025-05-13 02:57:25 +0100

    Update index.tsx add lang prop and sets lang attribute

--- src/components/blocks/TitleBlock/index.tsx
+++ src/components/blocks/TitleBlock/index.tsx
@@ -4,11 +4,14 @@ import classNames from 'classnames';
 import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
 
 export default function TitleBlock(props) {
-    const { className, text = [], color = 'text-dark', styles = {} } = props;
+    const { className, text = [], color = 'text-dark', styles = {}, lang } = props;
     const fieldPath = props['data-sb-field-path'];
     if (!text) {
         return null;
     }
+
+    // Determine if the content is Arabic based on lang prop
+    const isArabic = lang === 'ar';
 
     return (
         <h2
@@ -20,6 +23,7 @@ export default function TitleBlock(props) {
                 className,
                 styles?.self ? mapStyles(styles?.self) : undefined
             )}
+            lang={isArabic ? 'ar' : 'en'} // Set lang attribute dynamically
             data-sb-field-path={fieldPath}
         >
             <span {...(fieldPath && { 'data-sb-field-path': '.text' })}>{text}</span>
